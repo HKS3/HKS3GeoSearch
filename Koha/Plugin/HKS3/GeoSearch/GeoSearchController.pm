59d67b488fe24ac4b3480a365a045d3bde71fca0
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2024-05-17 12:57:49 +0200

    Clean up and update the API spec and the Controller code

--- Koha/Plugin/HKS3/GeoSearch/GeoSearchController.pm
+++ Koha/Plugin/HKS3/GeoSearch/GeoSearchController.pm
@@ -1,65 +1,32 @@
 package Koha::Plugin::HKS3::GeoSearch::GeoSearchController;
-
-use utf8;
+use strict;
+use warnings;
 use Mojo::Base 'Mojolicious::Controller';
 
-use C4::Context;
 use C4::Biblio;
-use C4::XSLT;
-
-use C4::Biblio;
-use C4::XSLT;
-
-use Koha::Biblios;
-use Koha::Items;
-use Mojo::JSON qw(decode_json encode_json);
-use Encode qw(encode_utf8);
 use MARC::File::XML ( DefaultEncoding => 'utf8' );
-
-sub post {
-    my $c = shift->openapi->valid_input or return;
-    my $biblionumbers = $c->validation->every_param('biblionumber');
-
-##     my $dbh = C4::Context->dbh;
-## 
-##     my $sql= <<'SQL';
-## with search as (
-## select authid, 
-##        ExtractValue(marcxml, '//datafield[@tag="035"]/subfield[@code="a"]')  as idn,
-##        ExtractValue(marcxml, '//datafield[@tag="150"]/subfield[@code="a"]') as fieldvalue,
-##        gnd_id nid
-##   from auth_header where authtypecode = 'GENRE/FORM' and origincode = 'rda')
-## select * from search where biblionumber in  ?
-## order by fieldvalue
-## SQL
-## 
-##     my $query = $dbh->prepare($sql);
-##     $query->execute($searchterm);
-##     my $items = $query->fetchall_arrayref({});
-
-    return $c->render( status => 200, openapi => {data => $biblionumbers, info => 'test'}  );
-}
-
 
 sub get {
     my $c = shift->openapi->valid_input or return;
     my $biblionumbers = $c->validation->every_param('bn');
-    my $data = [];
-    my $i = 0;
-    for my $b (@$biblionumbers) {
-        my $d = {};
-        my $marcxml =  C4::Biblio::GetXmlBiblio( $b );
+    my @data;
+
+    for my $biblio_number (@$biblionumbers) {
+        my $marcxml = C4::Biblio::GetXmlBiblio( $biblio_number );
         my $record  = MARC::Record->new_from_xml( $marcxml, 'UTF-8', 'MARC21' );
         if ( $record->field('034') ) { 
-          $d->{coordinates} = [$record->field('034')->subfield("s"), $record->field('034')->subfield("t")];
-          $d->{title}       = sprintf("<a href='/cgi-bin/koha/opac-detail.pl?biblionumber=%d'>%s</a>",
-                            $b, $record->field('245')->subfield("a"));
-          push @$data, $d;
-          $i++;
+            push @data, {
+                bn => $biblio_number,
+                coordinates => [ $record->field('034')->subfield("s"), $record->field('034')->subfield("t") ],
+                title => sprintf(
+                    "<a href='/cgi-bin/koha/opac-detail.pl?biblionumber=%d'>%s</a>",
+                    $biblio_number, $record->field('245')->subfield("a")
+                ),
+            };
         } 
     }
-    return $c->render( status => 200, openapi => {data => $data,  count => $i}  );
+
+    return $c->render( status => 200, openapi => { data => \@data, count => scalar(@data) });
 }
 
 1;
-
